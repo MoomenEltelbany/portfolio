a3e008253c3fa138d6a6671b704e4f1c58815b8e
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2025-10-06 16:47:16 +0300

    Remove the background color on the Card when hover

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -7,8 +7,8 @@ function About() {
 
     return (
         <main className="bg-slate-900 min-h-dvh flex justify-center items-center px-4 py-8 sm:px-6 md:px-10">
-            <section className="bg-slate-900 w-full max-w-3xl">
-                <div className="bg-slate-900">
+            <section className="bg-transparent w-full max-w-6xl ml-auto">
+                <div className="bg-transparent">
                     <ProfileCard
                         name="Moomen ELtelbany"
                         title="Front-End Developer"
@@ -21,7 +21,7 @@ function About() {
                         enableTilt={true}
                         enableMobileTilt={false}
                         onContactClick={() => navigate("/contact")}
-                        className="bg-transparent"
+                        className="bg-transparent flex justify-center hover:bg-transparent"
                     />
                 </div>
             </section>
